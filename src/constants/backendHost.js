d88b4eb23536d0d927bb757d284fa91f6a93b05f
Makinde Ayooluwa <makindeayooluwa604@gmail.com>
2026-03-24 13:49:31 +0100

    new changes

--- src/constants/backendHost.js
+++ src/constants/backendHost.js
@@ -1,2 +1,2 @@
-export const backendHost = "https://shopmmerce-backend-production.up.railway.app";
+export const backendHost = "https://shopmmerce-backend-production.up.railway.app/app";
 // export const backendHost = "http://localhost/shopmmerce-backend";
